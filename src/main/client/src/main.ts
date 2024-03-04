1fae1ff809a4e787c09c5ff0882eac37300a737b
Denartha10 <mahonniall2012@gmail.com>
2024-03-04 19:18:46 +0000

    Added some fixes and types to the typescript websocket file

---
+++ src/main/client/src/main.ts
@@ -0,0 +1,1 @@
+import { start } from "./websocketConnection";
--- src/main/client/src/websocketConnection.ts
+++ src/main/client/src/websocketConnection.ts
@@ -1,75 +1,85 @@
-import {Client, Message} from '@stomp/stompjs';
+import { Client } from "@stomp/stompjs";
 
-const websocketURL: string = 'ws://localhost:8080/gs-guide-websocket';
-const playerIDKey: string = 'playerID';
+const websocketURL: string = "ws://localhost:8080/gs-guide-websocket";
+const playerIDKey: string = "playerID";
 
 const stompClient = new Client({
-    brokerURL: websocketURL
-})
+  brokerURL: websocketURL,
+});
 
-const handleMove = (direction) => () => {
-    const playerID: string | null = localStorage.getItem(playerIDKey);
-    if (!playerID) {
-        console.error("Player ID not found");
-        return;
-    }else {
-        stompClient.publish({
-            destination: "/app/move",
-            body: JSON.stringify({'direction': direction, 'playerID': playerID})
-        });
-    }
+const handleMove = (direction: string) => () => {
+  const playerID: string | null = localStorage.getItem(playerIDKey);
+  if (!playerID) {
+    console.error("Player ID not found");
+    return;
+  } else {
+    const messageBody = JSON.stringify({
+      direction: direction,
+      playerID: playerID,
+    });
+
+    stompClient.publish({
+      destination: "/app/move",
+      body: messageBody,
+    });
+  }
 };
 
 const start = () => {
-    const playerNameInput = document.getElementById('playerName').value.trim();
+  const playerNameInput = document.getElementById(
+    "playerName",
+  ) as HTMLInputElement;
+  const trimmedNameInput = playerNameInput.value.trim();
 
-    if (playerNameInput.length < 3) {
-        alert("Name needs to be three characters long");
-    } else {
-        const startForm = document.getElementById('start-form');
-        startForm.classList.toggle("hidden");
+  if (trimmedNameInput.length < 3) {
+    alert("Name needs to be three characters long");
+  } else {
+    const startForm = document.getElementById("start-form") as HTMLElement;
+    startForm.classList.toggle("hidden");
 
-        stompClient.publish({
-            destination: "/app/initialise",
-        });
+    stompClient.publish({
+      destination: "/app/initialise",
+    });
 
-        document.addEventListener('keydown', (event) => {
-            switch (event.key) {
-                case 'ArrowLeft':
-                    handleMove('LEFT')();
-                    break;
-                case 'ArrowRight':
-                    handleMove('RIGHT')();
-                    break;
-                case 'ArrowUp':
-                    handleMove('UP')();
-                    break;
-                case 'ArrowDown':
-                    handleMove('DOWN')();
-                    break;
-                default:
-                    break;
-            }
-        });
-    }
+    document.addEventListener("keydown", (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          handleMove("LEFT")();
+          break;
+        case "ArrowRight":
+          handleMove("RIGHT")();
+          break;
+        case "ArrowUp":
+          handleMove("UP")();
+          break;
+        case "ArrowDown":
+          handleMove("DOWN")();
+          break;
+        default:
+          break;
+      }
+    });
+  }
 };
 
 stompClient.activate();
 
-stompClient.onConnect = (frame) => {
-    stompClient.subscribe("/topic/board", (frame) => {
-        const gameState = JSON.parse(frame.body);
-        const storedPlayerID = localStorage.getItem(playerIDKey);
+stompClient.onConnect = () => {
+  stompClient.subscribe("/topic/board", (frame) => {
+    const gameState = JSON.parse(frame.body);
+    const storedPlayerID = localStorage.getItem(playerIDKey);
 
-        if (gameState.playerID && !storedPlayerID) {
-            localStorage.setItem(playerIDKey, gameState.playerID);
-            console.log("Your ID is " + localStorage.getItem(playerIDKey));
-        }
+    if (gameState.playerID && !storedPlayerID) {
+      localStorage.setItem(playerIDKey, gameState.playerID);
+      console.log("Your ID is " + localStorage.getItem(playerIDKey));
+    }
 
-        console.log(gameState)
-    });
+    console.log(gameState);
+  });
 };
 
 stompClient.onWebSocketClose = () => {
-    localStorage.removeItem(playerIDKey);
+  localStorage.removeItem(playerIDKey);
 };
+
+export { start };
